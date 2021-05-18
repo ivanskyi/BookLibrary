95e35e3072528e1ea7ca14383abcf8180f6339b8
Oleh <olehfromukraine@gmail.com>
2021-05-18 22:41:41 +0300

    deleted some whitespaces

--- src/main/java/com/example/booklibrary/repository/AuthorRepository.java
+++ src/main/java/com/example/booklibrary/repository/AuthorRepository.java
@@ -4,7 +4,6 @@ import com.example.booklibrary.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
@@ -12,5 +11,4 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     @Query("FROM Author u WHERE u.authorName  LIKE CONCAT('%', :partName)")
     List<Author> findAllByAuthorNameContaining(@Param("partName") String partName);
-
 }
